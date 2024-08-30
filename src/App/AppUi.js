065e3ab1e41ed1fe5e8f638917a44e5795829edd
Jared <jaredozono@gmail.com>
2024-08-29 17:44:45 -0700

    posibl solucion de error en verion

--- src/App/AppUi.js
+++ src/App/AppUi.js
@@ -36,7 +36,7 @@ const {
         return (
           <>
             {sinTareas ? (
-              <h1>Agrega una nueva Tarea <br/> en botón + </h1>
+              <h1>Agrega una nueva Tarea <br/> en botón +</h1>
             ) : tareasCompletadas ? (
               <h1>Felicitaciones, completaste todas tus tareas.</h1>
             ) : (
